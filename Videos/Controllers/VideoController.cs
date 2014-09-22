e371075b279e13d7b36a23757b53e9234aa607e0
ptouchton <p_touchton@yahoo.com>
2014-09-21 17:57:59 -0700

    delete

---
+++ Videos/Controllers/VideoController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Videos.Controllers
+{
+    public class VideosController : ApiController
+    {
+        // GET api/video
+        public IEnumerable<string> GetAllVideos()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/video/5
+        public string Get(int id)
+        {
+            return "value" + " " + id.ToString();
+        }
+
+        // POST api/video
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/video/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/video/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
